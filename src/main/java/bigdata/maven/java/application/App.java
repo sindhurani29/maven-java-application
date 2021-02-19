444321f12e5f09b88b6e30e8b2f08502c64dd939
sindhu rani goli <saisindhurani@gmail.com>
2021-02-19 15:28:52 -0600

    Added files

---
+++ src/main/java/bigdata/maven/java/application/App.java
@@ -0,0 +1,13 @@
+package bigdata.maven.java.application;
+
+/**
+ * My Maven-Java first application
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
